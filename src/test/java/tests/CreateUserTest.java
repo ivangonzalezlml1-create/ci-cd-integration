7ffd760cf2ae798886ccb066408f83da6f3315fc
ivangonzalezlml1-create <ivan.gonzalezlml1@gmail.com>
2026-05-21 21:54:02 -0600

    Bad request

--- src/test/java/tests/CreateUserTest.java
+++ src/test/java/tests/CreateUserTest.java
@@ -25,7 +25,7 @@ public class CreateUserTest {
         User user = new User();
         user.setUsername(RandomUtils.getRandomAlphabeticString());
         user.setFirstName("Ivan");
-        user.setLastName("Gonzalez");
+        user.setLastName("Gonzalez Hernandez");
         user.setEmail(RandomUtils.getRandomAlphabeticString());
         user.setPassword(RandomUtils.getRandomAlphabeticString());
         user.setPhone(RandomUtils.getRandomAlphabeticString());
